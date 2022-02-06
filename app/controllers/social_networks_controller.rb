42d2e6488c5354ee09b35bba0a7720c36511ae80
Darlan Alaor Selzlein <sdarlanset@gmail.com>
2022-02-06 12:10:07 -0300

    Implementing social_networks#index action.

--- app/controllers/social_networks_controller.rb
+++ app/controllers/social_networks_controller.rb
@@ -1,5 +1,15 @@
 class SocialNetworksController < ApplicationController
   def index
-    render json: { message: 'it is working' }
+    threads = {
+      twitter: Thread.new { Thread.current[:result] = TwitterClient.new.tweets },
+      facebook: Thread.new { Thread.current[:result] = FacebookClient.new.statuses },
+      instagram: Thread.new { Thread.current[:result] = InstagramClient.new.photos },
+    }
+
+    response = {}
+
+    threads.each { |net, tr| response[net] = tr.join[:result] }
+
+    render json: response
   end
 end
